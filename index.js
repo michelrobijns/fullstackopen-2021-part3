5c094daa5c7aad470c66d636ab8dd921d6e34459
Michel Robijns <michelrobijns@users.noreply.github.com>
2021-09-18 19:27:39 +0200

    Add exercise 3.4 solution

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 
-const persons = [
+let persons = [
     {
       "id": 1,
       "name": "Arto Hellas",
@@ -43,6 +44,13 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
